2b3a735f029adacf3e619776db4ed205457205c4
Felipe Santana <felipesantanasantos1812@gmail.com>
2022-12-13 20:52:29 -0300

    acertandoReactNavigation

---
+++ screens/screenLogin.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+function Login({navigation}) {
+    return(
+        <View>
+            
+        </View>
+    )
+}
+
+export default Login
