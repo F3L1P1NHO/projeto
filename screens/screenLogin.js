a551cd0cf05c3b340afaf762f9c0f75ff5ed52dc
Felipe Santana <felipesantanasantos1812@gmail.com>
2022-12-14 11:04:55 -0300

    ColocandoAnimacao

--- screens/screenLogin.js
+++ screens/screenLogin.js
@@ -1,9 +1,53 @@
-import React from 'react';
-import { View } from 'react-native';
+import React,{useRef} from 'react';
+import { View,Animated,Image, TouchableHighlight,Text,Easing } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
+
+import styles from '../src/styles';
 
 function Login({navigation}) {
+
+
+    const startValue = useRef(new Animated.Value(0)).current;
+    const startValue2 = useRef(new Animated.Value(0)).current;
+
+  
+    startValue.setValue(-150);
+
+    Animated.timing(startValue, {
+      toValue: 30,
+      duration: 2000,
+      easing: Easing.linear,
+      useNativeDriver: true,
+    }).start();
+
+    startValue2.setValue(0)
+
+    Animated.timing(startValue2,{
+        toValue: 1,
+        duration: 4000
+    }).start()
+
     return(
-        <View>
+        <View style={styles.vw_login}>
+            <Animated.Image
+            source={require('../assets/logo.png')}
+            style={{width: 150,height: 150,transform:[{translateY: startValue,}]}}
+            />
+
+            <Animated.View style={[styles.vw_txInp,{opacity: startValue2}]}>
+                <TextInput
+                placeholder='Username'
+                style={styles.txInp}
+                />
+                <TextInput
+                placeholder='Password'
+                style={[styles.txInp,{marginTop: 20}]}
+                />
+            </Animated.View>
+            
+            <TouchableHighlight style={styles.btn_SIN}>
+                <Text style={{fontSize: 18}}>Sign In</Text>
+            </TouchableHighlight>
             
         </View>
     )
---
+++ src/styles.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {StyleSheet} from 'react-native'
+
+const styles = StyleSheet.create({
+  vw_login:{
+    flexGrow: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    backgroundColor: 'black',
+  },
+  txInp:{
+    borderRadius: 20,
+    borderColor: 'black',
+    width: '80%',
+    height: 50,
+    backgroundColor: 'red',
+    fontSize: 18,
+    borderWidth: 3,
+  },
+  vw_txInp:{
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor:'lightgray',
+    width: '80%',
+    height: 200,
+    marginTop: 50,
+    borderRadius: 20,
+  },
+  btn_SIN:{
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '80%',
+    height: 50,
+    backgroundColor: 'red',
+    borderRadius: 30,
+    marginTop: 20,
+    borderWidth: 3
+  }
+})
+
+export default styles
